c6670e95ed78a89f0518422902ef9b2d813f0727
sniperdevil <muge111hhh@gmail.com>
2020-02-28 08:07:42 +0530

    chages

---
+++ src/Components/PlayerBattle/BattleContainer.js
@@ -0,0 +1,33 @@
+// import React, { Component } from 'react';
+// import './BattleContainer.css';
+// // import Container from '../Container';
+// // import Container1 from '../Container1';
+// // import Container2 from '../Container2';
+// // import Container3 from '../Container3';
+
+// export default class BattleContainer extends Component {
+//     render() {
+//         return (
+//             <div>
+//                  <div className="wrapper">
+//         <div id="cont1">
+//           <Container player="2YL20Y0UP"/>
+//         </div>
+
+//          <div id="cont2">
+//            <Container1 />
+//          </div>
+
+//         <div id="cont3">
+//           <Container2 />
+//         </div>
+
+//         <div id="cont4">
+//           <Container3 />
+//         </div>
+//       </div>
+      
+//             </div>
+//         )
+//     }
+// }
